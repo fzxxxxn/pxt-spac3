cdc3e64b45bdf227c85e00b5cb0369fb965a3c7e
fzxxxxn <116843906+fzxxxxn@users.noreply.github.com>
2022-11-29 08:49:38 -0500

    Update spac3.ts

--- spac3.ts
+++ spac3.ts
@@ -8,38 +8,48 @@
 
 namespace spac3 {
     //% block
-  
+
     export function group() {
 
     }
     //% block
-    
-    
-    export function line(block: number){
-        
+
+
+    export function line(block: number) {
+
     }
-    
-    
+
+
     //% block="from%p3=minecraftCreatePosition|to %p4=minecraftCreatePosition|move x:%x|y:%y|z:%z"
     //% inlineInputMode=external
-    export function move1(p3: Position,p4: Position,x:number,y:number,z:number){
+    export function move1(p3: Position, p4: Position, x: number, y: number, z: number) {
         const p5 = positions.add(pos(x, y, z), p3)
         blocks.clone(
-        p3,
-        p4,
-        p5,
-        CloneMask.Replace,
-        CloneMode.Move
-    );}
+            p3,
+            p4,
+            p5,
+            CloneMask.Replace,
+            CloneMode.Move
+        );
+    }
     //% block="from%p0=minecraftCreatePosition|to %p1=minecraftCreatePosition|moveto %p2=minecraftCreatePosition||"
     //% inlineInputMode=external
-    export function moveto(p0: Position,p1: Position,p2: Position){
-        blocks.clone(
-        p0,
-        p1,
-        p2,
-        CloneMask.Replace,
-        CloneMode.Move
-    );
+    export function moveto(p0: Position, p1: Position, p2: Position) {
+        moveAll(p0,p1,p2);
     }
- }
+    //% block="from%p0=minecraftCreateWorldPosition to %p1=minecraftCreateWorldPosition|moveto %p2=minecraftCreateWorldPosition||"
+    //% inlineInputMode=external    
+    export function moveTrue(p0: Position, p1: Position, p2: Position) {
+        moveAll(p0,p1,p2);
+        
+    }
+    export function moveAll(p0: Position, p1: Position, p2: Position){
+         blocks.clone(
+            p0,
+            p1,
+            p2,
+            CloneMask.Replace,
+            CloneMode.Move);
+    }
+    export function calcuNewpos(p0: Position, p1: Position ){}
+}
